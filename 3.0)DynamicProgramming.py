114bfea3890933338cd39537ac2abb7c38537046
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-09-22 13:37:23 +0530

    Update 3.0)DynamicProgramming.py

--- 3.0)DynamicProgramming.py
+++ 3.0)DynamicProgramming.py
@@ -1,8 +1,7 @@
 def LCS(X , Y): 
     m = len(X) 
     n = len(Y) 
-  
-    # declaring the array for storing the dp values 
+   
     L = [[-1]*(n+1) for i in range(m+1)] 
 
     for i in range(m+1): 
@@ -15,6 +14,49 @@ def LCS(X , Y):
                 L[i][j] = max(L[i-1][j] , L[i][j-1])   
     return L[m][n] 
 
+def coinChange(coins, sum):
+    C = [[-1]*(len(coins)+1) for j in range(sum+1)]
+    for i in range(len(coins)+1):
+        C[0][i] = 1
+    for i in range(1, sum+1):
+        C[i][0] = 0
+
+    for i in range(1,sum+1):
+        for j in range(1, len(coins)+1):
+            C[i][j] = C[i][j-1]
+            if(coins[j-1]<=i):
+                C[i][j]+=C[i-coins[j-1]][j]
+    return C[sum][len(coins)] 
+
+def editDistance(str1, str2):
+    m = len(str1)
+    n = len(str2)
+
+    eD = [[-1]*(m+1) for i in range(n+1)]
+    for i in range(0,m+1):
+        eD[0][i] = i
+    for i in range(0,n+1):
+        eD[i][0] = i
+
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if(str1[j-1]==str2[i-1]):
+                eD[i][j] = eD[i-1][j-1]
+            else:
+                eD[i][j] = 1+min(eD[i-1][j], eD[i][j-1], eD[i-1][j-1])
+    return eD[n][m]
+
+#Longest common subsequence
 X = "AGGTAB"
 Y = "GXTXAYB"
-print("Length of LCS is ", LCS(X, Y))
+print("Length of LCS is", LCS(X, Y))
+
+#Coins Change
+coins = [1, 2, 3]  
+sum = 4
+print("Number of ways:",coinChange(coins, sum))
+
+#Edit Distance
+str1 = "sunday"
+str2 = "saturday"
+print("Operations:",editDistance(str1, str2))
