77183e5e6174eaebffc26ab4853159ee23ed96d9
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-09-23 17:44:46 +0530

    Update 3.0)DynamicProgramming.py

--- 3.0)DynamicProgramming.py
+++ 3.0)DynamicProgramming.py
@@ -46,6 +46,89 @@ def editDistance(str1, str2):
                 eD[i][j] = 1+min(eD[i-1][j], eD[i][j-1], eD[i-1][j-1])
     return eD[n][m]
 
+def LIS(arr):
+    n = len(arr)
+    lis = [1]*n
+    for i in range(0,n):
+        j=i-1
+        while(j>=0):
+            if(arr[i]<=arr[j]):
+                j-=1
+            else:
+                lis[i] = max(lis[i], 1+lis[j])
+                j-=1
+    return max(lis)
+
+def BetterLIS(arr):
+    n = len(arr)
+    tail = [None]*n
+    tail[0]=arr[0]
+    length=1
+    for ele in arr:
+        if(ele>tail[length-1]):
+            tail[length] = ele
+            length+=1
+        else:
+            ind = BinarySearchTail(tail,-1,length-1,ele)
+            tail[ind] = ele
+    return length
+
+def BinarySearchTail(arr, start, end, item):
+    while(end-start>1):
+        mid = start + (end-start)//2
+        if(arr[mid]<=item):
+            start = mid
+        else:
+            end = mid
+    return end
+
+def minCoins(arr, s):
+    n = len(arr)
+    mC = [float('inf')]*(s+1)
+    mC[0] = 0
+
+    for i in range(1, s+1):
+        for j in range(n):
+            if(i>=arr[j]):
+                temp = mC[i-arr[j]]
+                if(temp!=float('inf') and temp+1<mC[i]):
+                    mC[i] = temp+1
+    return mC[s]    
+
+def minJumps(arr):
+    n = len(arr)
+    if(arr[0]==0):
+        return float('inf')
+    if(n==0 or n==1):
+        return 0
+    jumps = [float('inf')]*n
+    jumps[0]=0
+    for i in range(1, n):
+        j=0
+        while(j<i):
+            if(j+arr[j]>=i):
+                if(jumps[j]!=float('inf')):
+                    jumps[i] = min(jumps[i], jumps[j]+1)
+                    break
+            j+=1
+    return jumps[n-1]
+
+def Knapsack(prices, weights, capacity):
+    n = len(prices)
+    dp = [[0]*(capacity+1) for i in range(n+1)]
+    for i in range(0,capacity+1):
+        dp[0][i] = 0
+    for i in range(0,n+1):
+        dp[i][0] = 0
+    
+    for i in range(1,n+1):
+        for j in range(1,capacity+1):
+            if(weights[i-1]<=j):
+                dp[i][j] = max(dp[i-1][j], prices[i-1]+dp[i-1][j-weights[i-1]])
+            else:
+                dp[i][j] = dp[i-1][j]
+    return dp[n][capacity]
+
 #Longest common subsequence
 X = "AGGTAB"
 Y = "GXTXAYB"
@@ -60,3 +143,22 @@ print("Number of ways:",coinChange(coins, sum))
 str1 = "sunday"
 str2 = "saturday"
 print("Operations:",editDistance(str1, str2))
+
+#Longest Increasing Subsequence
+arr = [10, 22, 9, 33, 21, 50, 41, 60] 
+print("Length of LIS:", LIS(arr))
+
+#Minimum coins change
+Coins = [9, 6, 5, 1]  
+V = 11
+print("Number of coins:",minCoins(Coins, V))
+
+#Minimum jumps to reach end
+arr = [1, 3, 6, 1, 0, 9] 
+print('Minimum Jumps:', minJumps(arr))
+
+#0/1 Knapsack Problem
+val = [60, 100, 120] 
+wt = [10, 20, 30] 
+W = 50 
+print("Value in Knapsack:",Knapsack(val, wt, W))
